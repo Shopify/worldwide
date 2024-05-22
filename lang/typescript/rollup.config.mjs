901a6ea8392a4a52f95e2c00c59f3d67a09db1d6
Kenny Goff <kenny.goff@shopify.com>
2024-05-22 20:13:10 +0000

    Initial setup of typescript package with rollup

---
+++ lang/typescript/rollup.config.mjs
@@ -0,0 +1,11 @@
+// rollup.config.js
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+    input: 'src/index.ts',
+    output: {
+        dir: 'output',
+        format: 'cjs'
+    },
+    plugins: [typescript()]
+};
